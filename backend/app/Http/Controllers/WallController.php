3328c488f4667787f0b1c741162909beb9663a7f
Rafael H Liost <rafael.lisot@unoesc.edu.br>
2021-01-13 19:20:21 -0300

    Colocar e tirar likes feito

--- backend/app/Http/Controllers/WallController.php
+++ backend/app/Http/Controllers/WallController.php
@@ -38,4 +38,33 @@ class WallController extends Controller
 
         return $array;
     }
+
+    public function like($id) {
+        $array = ['error' => ''];
+
+        $user = auth()->user();
+
+        $meLikes = WallLike::where('id_wall', $id)
+            ->where('id_user', $user['id'])
+            ->count();
+
+        if ($meLikes > 0) {
+            WallLike::where('id_wall', $id)
+                ->where('id_user', $user['id'])
+                ->delete();
+
+            $array['liked'] = false;
+        } else {
+            $newLike = new WallLike();
+            $newLike->id_wall = $id;
+            $newLike->id_user = $user['id'];
+            $newLike->save();
+
+            $array['liked'] = true;
+        }
+
+        $array['likes'] = WallLike::where('id_wall', $id)->count();
+
+        return $array;
+    }
 }
