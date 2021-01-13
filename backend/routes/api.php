8164724e7d53f4d5682a70f90c48edd2863490d1
Rafael H Liost <rafael.lisot@unoesc.edu.br>
2021-01-12 23:26:41 -0300

    Corrigindo erro que imposibilitava uso do midleware de autenticação

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -22,7 +22,7 @@ Route::get('/401', [AuthController::class, 'unauthorized'])->name('login');
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/register', [AuthController::class, 'register']);
 
-Route::middleware('auth-api')->group(function() {
+Route::middleware('auth:api')->group(function() {
     Route::post('/auth/validate', [AuthController::class, 'validateToken']);
     Route::post('/auth/logout', [AuthController::class, 'logout']);
 
