0b4baef77b9ec140b45b099214dd2132ac09ad2f
Rafael H Liost <rafael.lisot@unoesc.edu.br>
2021-01-13 22:08:52 -0300

    Lista negra geral criada

--- backend/app/Http/Controllers/WarningController.php
+++ backend/app/Http/Controllers/WarningController.php
@@ -4,7 +4,55 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Warning;
+use App\Models\Unit;
+
 class WarningController extends Controller
 {
-    //
+    public function getMyWarnigs(Request $request) {
+        $array = ['error' => ''];
+
+        $property = $request->input('property');
+
+        if ($property) {
+
+            $user = auth()->user();
+
+            $unit = Unit::where('id', $property)
+                ->where('id_owner', $user['id'])
+                ->count();
+
+            if ($unit > 0) {
+
+                $warnings = Warning::where('id_unit', $property)
+                    ->orderBy('datecreated', 'DESC')
+                    ->orderBy('id', 'DESC')
+                    ->get();
+
+                foreach ($warnings as $warnKey => $warnValue) {
+                    $warnings[$warnKey]['datecreated'] = date('d/m/y', strtotime($warnValue['datecreated']));
+                    $photoList = [];
+
+                    $photos = explode(',', $warnValue['photos']);
+                    foreach ($photos as $photo) {
+                        if (!empty($phoyos)) {
+                            $photoList[] = asset('storage'.$photo);
+                        }
+                    }
+
+                    $warnings[$warnKey]['photos'] = $photoList
+                }
+
+                $array['list'] = $warnings;
+
+            } else {
+                $array['error'] = 'Esta unidade não é sua!';
+            }
+
+        } else {
+            $array['error'] = 'A propriedade é obrigatória.';
+        }
+
+        return $array;
+    }
 }
