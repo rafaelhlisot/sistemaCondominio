edcbeb34252c8855c8cc77341a68ddf62a236325
Rafael H Liost <rafael.lisot@unoesc.edu.br>
2021-01-14 21:07:38 -0300

    Lista de achados e perdidos feita

--- backend/app/Http/Controllers/FoundAndLostController.php
+++ backend/app/Http/Controllers/FoundAndLostController.php
@@ -4,7 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\FoundAndLost;
+use FFI;
+
 class FoundAndLostController extends Controller
 {
-    //
+    public function getAll() {
+        $array = ['error' => ''];
+
+        $lost = FoundAndLost::where('status', 'LOST')
+            ->orderBy('datecreated', 'DESC')
+            ->orderBy('id', 'DESC')
+            ->get();
+
+        foreach ($lost as $lostKey => $lostValue) {
+            $lost[$lostKey]['datecreated'] = date('d/m/y', strtotime($lostValue['datecreated']));
+            $lost[$lostKey]['photo'] = asset('storage/'.$lostValue['photo']);
+        }
+
+        $array['lost'] = $lost;
+
+        $recovered = FoundAndLost::where('status', 'RECOVERED')
+            ->orderBy('datecreated', 'DESC')
+            ->orderBy('id', 'DESC')
+            ->get();
+
+        foreach ($recovered as $recKey => $recValue) {
+            $recovered[$recKey]['datecreated'] = date('d/m/y', strtotime($recValue['datecreated']));
+            $recovered[$recKey]['photo'] = asset('storage/'.$recValue['photo']);
+        }
+
+        $array['recovered'] = $recovered;
+
+        return $array;
+    }
 }
