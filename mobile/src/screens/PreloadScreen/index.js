02d8095905f678862778bcfb5179497fb37a623a
Rafael H Liost <rafael.lisot@unoesc.edu.br>
2021-01-27 21:28:38 -0300

    Criação de context, provider, e primeira tela

---
+++ mobile/src/screens/PreloadScreen/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import C from './style';
+
+export default () => {
+    return (
+        <C.container>
+            <C.LoadingIcon color="#8863E6" size="large" />
+        </C.container>
+    );
+}
---
+++ mobile/src/screens/PreloadScreen/style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export default {
+    container: styled.SafeAreaView`
+        flex: 1;
+        justify-content: center;
+        align-items: center;
+    `,
+
+    LoadingIcon: styled.ActivityIndicator`
+    
+    `
+};
